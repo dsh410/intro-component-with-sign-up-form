3cb31c69735cc4ac40216b263c5ce3e91c2e3ba3
dsh410 <dsh410@gmail.com>
2021-07-02 02:52:12 -0500

    changed msg according to design file.

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ for (let input of nameInput) {
 
 for (let input of nameInput) {
   input.addEventListener('invalid', () => {
-    if(input.value === '') {
-      input.setCustomValidity(`Enter your ${input.placeholder}!`);
+    if (input.value === '') {
+      input.style.border = '2px solid red'
+      input.setCustomValidity(`${input.placeholder} cannot be empty`);
     } else {
       input.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
     }
