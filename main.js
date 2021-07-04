8d532f1f7cd73206a9621ef4e640b35aaff5eeb2
dsh410 <dsh410@gmail.com>
2021-07-04 07:47:59 -0500

    using from data.

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 const nameInput = document.querySelectorAll('input');
+const form = document.querySelector('form');
+
+ let formData = new FormData(form)
+
+const request = new XMLHttpRequest();
+
 
 
 for (let input of nameInput) {
   input.addEventListener('input', () => {
     input.setCustomValidity('');
     input.checkValidity();
+    submitForm(input);
   });
 }
 
+function submitForm(input) {
+  const request = new XMLHttpRequest();
+  request.open("POST", "submitform.php");
+  request.send(new FormData(form));
+}
 
 
 
@@ -18,8 +30,22 @@ for (let input of nameInput) {
       input.style.border = '2px solid red'
       input.setCustomValidity(`${input.placeholder} cannot be empty`);
     } else {
-      input.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
+      input.setCustomValidity(`Looks like this is not an ${input.placeholder}`);
     }
   });
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
